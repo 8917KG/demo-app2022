276a2309bf984db974be1b4714bb5c8063894d66
8917KG <8917@ait.nsw.edu.au>
2022-11-13 10:49:34 +1100

    adding firebase and firebase-tools

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB-vr3ORfCqKU3D61STObEftiQVuqiKGTk",
+    authDomain: "bookreview-c44d4.firebaseapp.com",
+    projectId: "bookreview-c44d4",
+    storageBucket: "bookreview-c44d4.appspot.com",
+    messagingSenderId: "39959595995",
+    appId: "1:39959595995:web:6f5cfba139f6c8a19175fe"
+  };
